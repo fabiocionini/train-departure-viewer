e40f7db6b27449699c22449d9d63c73e9f9b54e6
Fabio Cionini <fabio.cionini@gmail.com>
2021-04-07 17:22:02 +0200

    docker cfg for frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  rootUrl: 'http://localhost:8080/',
+  apiPrefix: 'api/',
+  apiKey: 'APIKEY',
+  apiVersion: '1.0',
+  apiMimeType: 'application/vnd.fabiocionini.departures+json',
+  apiAuthorization: false,
+  refreshDataRate: 10,
+  pageCycleRate: 10,
+  boardingTime: 300,
+  departuresPerPage: 10,
 };
